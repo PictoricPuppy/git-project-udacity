e114dea322cdaf19e6e0f9afc02a80be9c401b31
ESantamaria <eva.santamaria.lopez@gmail.com>
2022-12-08 00:38:24 +0100

    refactor: Add description to restart function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -197,6 +197,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
+        #Here the script will ask to the user to restart or to stop the data treatment.
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
